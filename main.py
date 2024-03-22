b47c282f5b27bca557dd76c471ca54db54094460
Eric <ercx1023@gmail.com>
2024-03-22 19:13:54 -0400

    initialized some pygame constants and added config file

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+import os
+import sys
+
+pygame.init()
+
+# Global Constants
+SCREEN_HEIGHT = 720
+SCREEN_WIDTH = 1280
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
